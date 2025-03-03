1d63ac5bcc7b7903ea777c55fd2573ad9c66899e
Luis Velasquez <l.velasquez1308@gmail.com>
2025-03-03 15:26:31 -0600

    Basic project done

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -7,7 +7,7 @@ const CatalogPage: React.FC = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white">
       <h1 className="text-3xl font-bold">Catálogo de Personajes</h1>
       <p className="mt-4">Aquí mostraremos los personajes de Marvel API</p>
-      <Link to="/" className="mt-6 px-4 py-2 bg-red-600 rounded-lg">
+      <Link to="/" className="mt-6 px-4 py-2 bg-yellow-500 rounded-lg">
         Volver
       </Link>
     </div>
